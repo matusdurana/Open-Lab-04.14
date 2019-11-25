48a845a865699ea3c48b9592435911fdef8977dd
MatusDurana <matusdurana2@gmail.com>
2019-11-25 10:10:29 +0100

    done.

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,28 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            var lword = strings.OrderByDescending(s => s.Length).First().Length;
+            for (var a = 0; a < lword + 4; a++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine("");
+            foreach (var str in strings)
+            {
+                Console.Write($"* {str}");
+                for (var b = 1; b <= lword + 1 - str.Length; b++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                Console.WriteLine("");
+            }
+            for (var a = 0; a < lword + 4; a++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
         }
     }
 }
